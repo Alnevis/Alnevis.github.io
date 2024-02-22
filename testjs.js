cdbee883ec253ce12896c8c6ca9738046cfbea73
Alnevis <104582680+Alnevis@users.noreply.github.com>
2024-02-22 23:55:21 +0700

    Add files via upload

--- testjs.js
+++ testjs.js
@@ -32,7 +32,7 @@ let tg = window.Telegram.WebApp; //получаем объект webapp теле
    });
 
    Telegram.WebApp.onEvent('mainButtonClicked', function(){
-      tg.sendData(`Ваш ${tg.initDataUnsafe.user.first_name}`);
+      tg.sendData(`Ваш`);
       window.alert(`HELLO!`);
       //при клике на основную кнопку отправляем данные в строковом виде  
    });
