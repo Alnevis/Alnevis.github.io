04d83b41e35e9557fc067368c4da149a08fc555d
Alnevis <104582680+Alnevis@users.noreply.github.com>
2024-02-22 19:10:12 +0700

    Update testjs.js

--- testjs.js
+++ testjs.js
@@ -32,8 +32,9 @@ let tg = window.Telegram.WebApp; //получаем объект webapp теле
    });
 
    Telegram.WebApp.onEvent('mainButtonClicked', function(){
-      tg.sendData(`Ваши имя сохранено, ${tg.initDataUnsafe.user.first_name}`);
-      //при клике на основную кнопку отправляем данные в строковом виде
+      tg.sendData(`Ваши имя сохранено111`);
+      window.alert(`HELLO!`);
+      //при клике на основную кнопку отправляем данные в строковом виде ${tg.initDataUnsafe.user.first_name}
    });
 
 
