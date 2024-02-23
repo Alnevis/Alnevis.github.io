516343b255f61e36c345c6048f8c190d51929ccd
Alnevis <104582680+Alnevis@users.noreply.github.com>
2024-02-23 23:52:58 +0700

    Update mycafe.js

--- mycafe.js
+++ mycafe.js
@@ -1,18 +1,19 @@
 let tg = window.Telegram.WebApp; //получаем объект webapp телеграма
-let x = tg.initData;
+let x = tg.initDataUnsafe;
+let z = false;
    tg.expand(); //расширяем на все окно
   
    tg.MainButton.text = "Changed Text"; //изменяем текст кнопки
    tg.MainButton.setText("ЗАКАЗАТЬ"); //изменяем текст кнопки иначе ${x}
    tg.MainButton.show()
-   tg.SettingsButton.hide()
+   tg.SettingsButton.show()
    tg.isClosingConfirmationEnabled = false;
    tg.BackButton.show()
 
      
 
    Telegram.WebApp.onEvent('mainButtonClicked', function () {
-    if (!Telegram.WebApp.initDataUnsafe) {
+    if (!z) {
       tg.sendData(`Ваш Заказ отправлен! `);
       
        
