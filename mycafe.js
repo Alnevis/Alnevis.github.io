1b844f0cca5071deb1d9991846ca277b01ab8f86
Alnevis <yabest125@mail.ru>
2024-02-24 14:37:28 +0700

    3

--- mycafe.js
+++ mycafe.js
@@ -20,57 +20,49 @@ Telegram.WebApp.onEvent('mainButtonClicked', function () {
       tg.showAlert(`!!!`);
   }
 });
-var addbutton = $('.js-item-incr-btn');
-// Select the quantity decrement button
-// Add a click event listener to the button 
-addbutton.on('click', function(event) {
-event.preventDefault();
 
-var itemEl = $(this).closest('.js-item');
-incrClicked(itemEl, 1);
+var addbutton = $('.js-item-incr-btn');
+ 
+addbutton.on('click', function(event) {
+  event.preventDefault();
+  var itemEl = $(this).closest('.js-item');
+  incrClicked(itemEl, 1);
 });
 
-
-
-
 var decrButton = $('.js-item-quantity-decr');
-
 // Add a click event listener to the button
 decrButton.on('click', function(event) {
-event.preventDefault();
-
-var itemEl = $(this).closest('.js-item');
-incrClicked(itemEl, -1);
+  event.preventDefault();
+  var itemEl = $(this).closest('.js-item');
+  incrClicked(itemEl, -1);
 });
 
 // Select the quantity increment button
 var incrButton = $('.js-item-quantity-incr');
-
 // Add a click event listener to the button
 incrButton.on('click', function(event) {
-event.preventDefault();
-
-var itemEl = $(this).closest('.js-item');
-incrClicked(itemEl, 1);
+  event.preventDefault();
+  var itemEl = $(this).closest('.js-item');
+  incrClicked(itemEl, 1);
 });
 
 // Update the item quantity display
 function updateItemQuantity(itemEl) {
-var count = itemEl.data('item-count');
-itemEl.find('.cafe-item-quantity').text(count);
-}
+  var count = itemEl.data('item-count');
+  itemEl.find('.cafe-item-quantity').text(count);
+};
 
 // Update the item
 function updateItem(itemEl, delta) {
-var count = itemEl.data('item-count') || 0;
-count += delta;
-if (count < 0) {
-  count = 0;
-}
-itemEl.data('item-count', count);
-updateItemQuantity(itemEl);
-// Add your custom logic here to update the item quantity in the order overview section
-}
+  var count = itemEl.data('item-count') || 0;
+  count += delta;
+  if (count < 0) {
+      count = 0;
+  }
+  itemEl.data('item-count', count);
+  updateItemQuantity(itemEl);
+  // Add your custom logic here to update the item quantity in the order overview section
+};
 
       
   
