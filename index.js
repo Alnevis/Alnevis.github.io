384a261171f5829060983d1c392f17d5bbebef25
Alnevis <yabest125@mail.ru>
2024-02-24 10:59:39 +0700

    1

---
+++ index.js
@@ -0,0 +1,16 @@
+//window.alert(`HELLO!`)
+//comment
+/* this 
+is ]
+comment
+*/
+document.getElementById("H1").textContent = `Hello Friend`;
+let x = 10;
+console.log(`hi you have ${x}`);
+var elements = document.querySelectorAll('.example');
+
+    // Iterate through the NodeList and apply styles
+    elements.forEach(function(element) {
+      element.style.fontWeight = 'bold';
+      
+    });
