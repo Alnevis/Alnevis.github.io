e42126450a997c731f4896a099642746cd60b634
Alnevis <yabest125@mail.ru>
2024-02-24 16:56:24 +0700

    4

--- mycafe.js
+++ mycafe.js
@@ -15,11 +15,11 @@ Telegram.WebApp.onEvent('mainButtonClicked', function () {
   if (!x) {
     //window.alert('x is true!');
     //window.alert(`ОТКРОЙ ПРИЛОЖЕНИЕ ЧЕРЕЗ КНОПКУ НА КЛАВИАТУРЕ БОТА!!!`);
-    tg.showAlert(`ОТКРОЙТЕ ПРИЛОЖЕНИЕ ЧЕРЕЗ КНОПКУ НА КЛАВИАТУРЕ БОТА!!!`);
+    tg.showAlert(`ОТКРОЙТЕ ПРИЛОЖЕНИЕ ЧЕРЕЗ КНОПКУ НА КЛАВИАТУРЕ БОТА!!! x: ${typeof x} ${JSON.stringify(x)}`);
     //tg.openTelegramLink('https://t.me/public_python');  
   } else {
       tg.showAlert(`Данные получены!`);
-      tg.sendData(`Ваш Заказ отправлен! x: ${typeof x} ${JSON.stringify(x)} y: ${typeof y} ${JSON.stringify(y)}`);     
+      tg.sendData(`Ваш Заказ отправлен!  y: ${typeof y} ${JSON.stringify(y)}`);     
     }
 });
 /*
