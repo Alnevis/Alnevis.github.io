59cac3ae3d069866db58b0fbfbf54af2f431e617
Alnevis <yabest125@mail.ru>
2024-02-27 14:16:21 +0700

    333

--- data.js
+++ data.js
@@ -125,17 +125,16 @@ return [newItemItem,newOrderItem];
 
 // Storing div information
 function storeDivInfo(itemName, price, description) {
-  tg.showAlert(`SAVED IN STORAGE`); 
+ 
   const divInfo = [itemName, price, description];
-  const jsonString = JSON.stringify(divInfo);
-  tg.showAlert(`SAVED IN STORAGE2`);
+  const jsonString = JSON.stringify(divInfo); 
+  tg.showAlert(`SAVED IN STORAGE`);  
   tg.CloudStorage.setItem(`${itemName}`, jsonString, function(error, success) {
     if (error) {
       tg.showAlert('Error storing data in Cloud Storage: ' + error);
     } else {
       tg.showAlert('Data stored successfully: ' + jsonString);
-    }
-    tg.showAlert(`SAVED IN STORAGE3`);
+    }    
   });
 }
 /*
