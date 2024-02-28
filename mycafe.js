50e8742eaa230fcf18e5f135dc69d6ce9328f234
Alnevis <yabest125@mail.ru>
2024-02-28 22:35:44 +0700

    17

--- mycafe.js
+++ mycafe.js
@@ -21,9 +21,9 @@ tg.MainButton.text = "Посмотреть заказ"; //изменяем те�
 //console.log('Hiding mc1:', $('.js-order-item.mc1'));
 $(document).ready(function() {
   console.log("I am here")
-  $('.cafe-block .cafe-order-item .js-order-item').hide();
+ // $('.cafe-block .cafe-order-item .js-order-item').hide();
 });
-logWithTimestamp(` 15 version bot api${tg.version} `); ////////////////////${tg.WebAppInitData.user}///////////////////////////////////////////////////////////////////////////////////////////
+logWithTimestamp(` 17 version bot api${tg.version} `); ////////////////////${tg.WebAppInitData.user}///////////////////////////////////////////////////////////////////////////////////////////
 tg.SettingsButton.show();
 tg.isClosingConfirmationEnabled = true;
 tg.BackButton.hide();
@@ -61,7 +61,7 @@ function incrClicked (itemEl, delta, itemId) {
   totalitemprice =totalitemprice.toFixed(2)
   console.log(" ITEMtitle", itemtitle , " totalitemprice", totalitemprice, " itempricefloat", itempricefloat )
   if (count > 0) {    
-    $('.cafe-block  .cafe-order-item .js-order-item#' + itemId+ "mc1").show();
+    $('.cafe-block  .js-order-item#' + itemId+ "mc1").show();
     //console.log(' oneitemprice:', $(`.oneitemprice#`+ itemId).text());
        
     itemprice = itemEl.find('.cafe-item-price').text();
@@ -102,7 +102,7 @@ function incrClicked (itemEl, delta, itemId) {
 
   } else {
     
-    $('.cafe-block  .cafe-order-item .js-order-item#' + itemId + "mc1").hide();
+    $('.cafe-block .js-order-item#' + itemId + "mc1").hide();
     itemEl.find('.js-item-quantity-incr').hide();
     itemEl.find('.js-item-quantity-decr').hide();
     itemEl.find('.js-item-quantity').hide();
