1dac37fc9b85052e29257ff5af03f25b4a1b83d2
Alnevis <104582680+Alnevis@users.noreply.github.com>
2024-03-02 22:39:04 +0700

    Add files via upload

--- testjs.js
+++ testjs.js
@@ -1,16 +1,17 @@
 let tg = window.Telegram.WebApp; //получаем объект webapp телеграма 
 tg.MainButton.show()
 tg.MainButton.enable()
-  
+  console.log("start")
    function getBotInfo() {
       // Replace 'YOUR_BOT_TOKEN' with your actual bot token
       const botToken = '6566526748:AAEKT_vmAfS5IXEvw7vJcX8qri-4YtUHGJ4';
-  
+      console.log("start1")
       // Telegram Bot API endpoint for the getMe method
       const apiUrl = `https://api.telegram.org/bot${botToken}/getMe`;
-  
+      console.log("start2")
       // Make a fetch request to the Telegram Bot API
       fetch(apiUrl)
+      console.log("start3")
           .then(response => response.json())
           .then(data => {
               console.log(data);
@@ -21,6 +22,7 @@ tg.MainButton.enable()
               alert('Error getting bot information. Check console for details.');
           });
   }
+  console.log("start6")
   Telegram.WebApp.onEvent('mainButtonClicked', function(){
    let profName = tg.initDataUnsafe.user.first_name   
     console.log(profName)
